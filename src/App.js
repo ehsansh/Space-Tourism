962acbf503716c9052f0fdcb5a4f5c54c4973a15
Ehsan Shadmehri <ehsanshadmehri@gmail.com>
2023-01-14 20:36:04 +0330

    add react-router-dom, add Home, add App.scss, give styles for design system

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import React from 'react';
-
-import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import { lazy, Suspense } from 'react';
+import './App.scss';
+const Home = lazy(() => import('./components/Home'));
 
 function App() {
-    return <div className='App'></div>;
+    return (
+        <div className='App'>
+            <Suspense>
+                <Routes>
+                    <Route path='/' element={<Home />} />
+                </Routes>
+            </Suspense>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Home</h1>
+            <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Provident, asperiores!
+            </p>
+            <h5>this is h5</h5>
+        </div>
+    );
+};
+
+export default Home;

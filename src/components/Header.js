7f7fa2ddba978af1210856f0525100c40ae553de
Ehsan Shadmehri <ehsanshadmehri@gmail.com>
2023-01-15 14:18:50 +0330

    add backgroun-image for home and add background for ul in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './styles/Header.scss';
+
 const Header = ({ active }) => {
     const liList = ['HOME', 'DESTINATION', 'CREW', 'TECHNOLOGY'];
     return (
         <header>
             <nav>
+                <div className='logo'>
+                    <img src={require('./images/logo.png')} alt='space logo' />
+                </div>
+                <div className='line'></div>
                 <ul>
                     {liList.map((val, i) => (
-                        <li className={active === val ? 'active' : ''}>
+                        <li key={i} className={active === val ? 'active' : ''}>
                             <Link className='' to='/'>
                                 <span className='num'>0{i}</span>
                                 <span className='text'>{val}</span>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import Header from './Header';
+import './styles/Home.scss';
 const Home = () => {
     return (
-        <>
+        <div className='Home'>
             <Header active='HOME' />
             <div>
-                <h1>Home</h1>
+                <h1>Space</h1>
                 <p>
                     Lorem ipsum dolor sit, amet consectetur adipisicing elit.
                     Provident, asperiores!
                 </p>
                 <h5>this is h5</h5>
             </div>
-        </>
+        </div>
     );
 };
 

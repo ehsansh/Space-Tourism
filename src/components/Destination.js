7c06bdbc59aab05258fa296b930ce2d1b586f527
Ehsan Shadmehri <ehsanshadmehri@gmail.com>
2023-01-16 12:42:01 +0330

    finish destination page

--- src/components/Destination.js
+++ src/components/Destination.js
@@ -5,14 +5,11 @@ import './styles/Destination.scss';
 
 import { destinations } from './data/data';
 
-console.log(destinations);
-
 const Destination = () => {
     const [currectDestination, setCurrentDestination] = useState('Moon');
+    const [change, setChange] = useState('');
 
     let data = destinations.filter(d => d.name === currectDestination)[0];
-
-    console.log(data);
 
     return (
         <div className='Destination'>
@@ -23,6 +20,7 @@ const Destination = () => {
                         <span className='num'>01</span>
                         PICK YOUR DESTINATION
                     </h1>
+                    <img src={require(`${data.images.png}`)} alt={data.name} />
                 </div>
                 <section className='intro'>
                     <ul className='menu'>
@@ -40,17 +38,19 @@ const Destination = () => {
                             </li>
                         ))}
                     </ul>
-                    <h2>{data.name}</h2>
-                    <p>{data.description}</p>
-                    <hr />
-                    <div className='details'>
-                        <div>
-                            <div className='subheading2'>Average distance</div>
-                            <div className='subheading1'>{data.distance}</div>
-                        </div>
-                        <div>
-                            <div className='subheading2'>est. travel time</div>
-                            <div className='subheading1'>{data.travel}</div>
+                    <div className='intro-content'>
+                        <h2>{data.name}</h2>
+                        <p>{data.description}</p>
+                        <hr />
+                        <div className='details'>
+                            <div>
+                                <div className='property'>Average distance</div>
+                                <div className='value'>{data.distance}</div>
+                            </div>
+                            <div>
+                                <div className='property'>est. travel time</div>
+                                <div className='value'>{data.travel}</div>
+                            </div>
                         </div>
                     </div>
                 </section>
